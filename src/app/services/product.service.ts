8381c68637bf8d3b5bcf9180b262fd625bfa5d79
Fabiola Barone <fabiolabarone.fb94@gmail.com>
2025-05-13 23:55:27 +0200

    carrello component

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -40,6 +40,14 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(thePage: number,
+                        thePageSize:number,
+                        theKeyWord: string): Observable<GetResponseProducts> {
+    //
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient
       .get<GetResponseProducts>(searchUrl)
